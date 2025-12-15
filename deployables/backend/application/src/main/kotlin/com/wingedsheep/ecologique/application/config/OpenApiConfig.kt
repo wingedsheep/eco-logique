22aa5222d8ee077d4c9902152d614ed165c3e59b
vincent <wingedsheep89@gmail.com>
2025-12-15 23:38:47 +0100

    feat(api): integrate OAuth2 security scheme in OpenAPI configuration

--- deployables/backend/application/src/main/kotlin/com/wingedsheep/ecologique/application/config/OpenApiConfig.kt
+++ deployables/backend/application/src/main/kotlin/com/wingedsheep/ecologique/application/config/OpenApiConfig.kt
@@ -1,21 +1,48 @@
 package com.wingedsheep.ecologique.application.config
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
-import io.swagger.v3.oas.annotations.info.Info
-import io.swagger.v3.oas.annotations.security.SecurityRequirement
-import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.OAuthFlow
+import io.swagger.v3.oas.models.security.OAuthFlows
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@OpenAPIDefinition(
-    info = Info(title = "Ecologique API", version = "v1"),
-    security = [SecurityRequirement(name = "bearer-key")]
-)
-@SecurityScheme(
-    name = "bearer-key",
-    type = SecuritySchemeType.HTTP,
-    scheme = "bearer",
-    bearerFormat = "JWT"
-)
-class OpenApiConfig
+class OpenApiConfig(
+    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private val issuerUri: String
+) {
+
+    @Bean
+    fun openAPI(): OpenAPI {
+        val tokenUrl = "$issuerUri/protocol/openid-connect/token"
+
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Ecologique API")
+                    .version("v1")
+                    .description("API for eco-friendly product management")
+            )
+            .components(
+                Components()
+                    .addSecuritySchemes(
+                        "oauth2",
+                        SecurityScheme()
+                            .type(SecurityScheme.Type.OAUTH2)
+                            .flows(
+                                OAuthFlows()
+                                    .password(
+                                        OAuthFlow()
+                                            .tokenUrl(tokenUrl)
+                                    )
+                            )
+                    )
+            )
+            .addSecurityItem(SecurityRequirement().addList("oauth2"))
+    }
+}
